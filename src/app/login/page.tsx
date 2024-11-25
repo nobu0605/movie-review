0d918aae993c4540930d203797a5318a23b9d9f1
Takanobu Suzuki <nobublack555@gmail.com>
2024-11-24 20:44:30 -0800

    feature/login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import { LoginForm } from '@/features/login/components/LoginForm'
+
+export default function Login() {
+  return (
+    <div className='flex justify-center items-center'>
+      <div className='flex flex-col w-2/5 gap-2.5 h-2 mt-8'>
+        <h1 className='text-4xl font-bold text-center'>Login</h1>
+        <LoginForm />
+      </div>
+    </div>
+  )
+}
---
+++ src/features/login/components/LoginForm.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import { useActionState } from 'react'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { login } from '@/features/login/serverActions/login'
+
+export function LoginForm() {
+  const [state, action, isPending] = useActionState(login, undefined)
+
+  return (
+    <form action={action}>
+      <div className='flex flex-col gap-2'>
+        <div>
+          <Label htmlFor='email'>Email</Label>
+          <Input id='email' name='email' placeholder='example@gmail.com' />
+        </div>
+        {state?.errors?.email && <p className='text-sm text-red-500'>{state.errors.email}</p>}
+        <div>
+          <Label htmlFor='password'>Password</Label>
+          <Input id='password' name='password' type='password' />
+        </div>
+        {state?.errors?.password && (
+          <div className='text-sm text-red-500'>
+            <p>Password must:</p>
+            <ul>
+              {state.errors.password.map((error: string) => (
+                <li key={error}>- {error}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+        <span className='text-sm text-red-500'>{state?.message}</span>
+        <Button aria-disabled={isPending} type='submit' className='mt-2 w-full'>
+          {isPending ? 'Submitting...' : 'Login'}
+        </Button>
+      </div>
+    </form>
+  )
+}
