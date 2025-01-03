6d2d79db7d467d92e0ad9f8afc31e58c8ccb50f8
Takanobu Suzuki <nobublack555@gmail.com>
2025-01-03 11:03:06 -0800

    fix/refactor

--- src/features/movie/helpers/movie.ts
+++ src/features/movie/helpers/movie.ts
@@ -1,53 +1,36 @@
 import 'server-only'
+import { Prisma } from '@prisma/client'
 import {
   Movie,
   MovieResult,
   MovieDetail,
-  MovieReviewFromDB,
+  AverageRating,
   Review,
+  Favorite,
 } from '@/features/movie/types/movie'
 import prisma from '@/utils/api/db'
 import { verifySession } from '@/utils/api/jwt'
 
+const movieRequestOptions = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`,
+  },
+}
+
 export async function getMovie(movieId: number): Promise<MovieDetail | null> {
   const session = await verifySession()
-  if (!session.isAuth) return null
+  if (!session.isAuth || !session.userId) return null
 
   const url = `https://api.themoviedb.org/3/movie/${movieId}`
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`,
-    },
-  }
-
-  const res = await fetch(url, { ...options, cache: 'force-cache' })
+  const res = await fetch(url, { ...movieRequestOptions, cache: 'force-cache' })
   const movie: MovieResult | null = await res.json()
   if (!movie) return null
 
-  const favorites = await prisma.favorite.findMany({
-    select: {
-      id: true,
-      movieId: true,
-      userId: true,
-    },
-    where: {
-      movieId,
-      userId: session.userId,
-    },
-  })
-
-  const averageRating: MovieReviewFromDB[] = await prisma.$queryRaw`
-    SELECT 
-      "movieId",
-      AVG("rating") AS "averageRating"
-    FROM "Review"
-    WHERE "movieId" = ${movieId}
-    GROUP BY "movieId";
-  `
-
-  const reviews: Review[] | null = await prisma.review.findMany({
+  const favorites = await getFavorites(session.userId, movieId)
+  const averageRating: AverageRating[] = await getAverageRatings(movieId)
+  const reviews: Review[] = await prisma.review.findMany({
     select: {
       id: true,
       movieId: true,
@@ -80,50 +63,75 @@ export async function getMovie(movieId: number): Promise<MovieDetail | null> {
 
 export async function getMovies(page: number): Promise<MovieDetail[] | null> {
   const session = await verifySession()
-  if (!session.isAuth) return null
+  if (!session.isAuth || !session.userId) return null
 
   const url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=popularity.desc`
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`,
-    },
-  }
-
-  const res = await fetch(url, { ...options, cache: 'force-cache' })
+  const res = await fetch(url, { ...movieRequestOptions, cache: 'force-cache' })
   const movie: Movie = await res.json()
   if (!movie) return null
   const movieResults = movie.results
 
   const movieIds: number[] = movieResults.map((movie) => movie.id)
-  const favorites = await prisma.favorite.findMany({
-    select: {
-      id: true,
-      movieId: true,
-      userId: true,
-    },
-    where: {
-      movieId: {
-        in: movieIds,
-      },
-      userId: session.userId,
-    },
-  })
+  const favorites = await getFavorites(session.userId, undefined, movieIds)
+  const averageRatings: AverageRating[] = await getAverageRatings(movieIds)
+  const reviews: Review[] = await getMovieReviews(movieIds)
+  const movieDetails = buildMovieDetails(movieResults, favorites, reviews, averageRatings)
 
-  const averageRatings: MovieReviewFromDB[] = await prisma.$queryRaw`
+  return movieDetails
+}
+
+export async function getFavoriteMovies(): Promise<MovieDetail[] | null> {
+  const session = await verifySession()
+  if (!session.isAuth || !session.userId) return null
+
+  const favorites = await getFavorites(session.userId)
+  const favoriteMovieIds = favorites.map((favorite) => favorite.movieId)
+  let favoriteMovies: MovieResult[] = []
+
+  try {
+    const movieRequests = favoriteMovieIds.map((movieId) =>
+      fetch(`https://api.themoviedb.org/3/movie/${movieId}`, {
+        ...movieRequestOptions,
+        cache: 'force-cache',
+      }).then((res) => res.json()),
+    )
+    favoriteMovies = await Promise.all(movieRequests)
+  } catch (error) {
+    console.error('Error fetching movies:', error)
+  }
+
+  if (favoriteMovies.length === 0) return null
+
+  const averageRatings: AverageRating[] = await getAverageRatings(favoriteMovieIds)
+  const reviews: Review[] | null = await getMovieReviews(favoriteMovieIds)
+  const movieDetails = buildMovieDetails(favoriteMovies, favorites, reviews, averageRatings)
+
+  return movieDetails
+}
+
+async function getAverageRatings(movieIdOrIds: number | number[]): Promise<AverageRating[]> {
+  const condition = Prisma.sql`${
+    Array.isArray(movieIdOrIds)
+      ? Prisma.sql`"movieId" = ANY (ARRAY[${movieIdOrIds}]::integer[])`
+      : Prisma.sql`"movieId" = ${movieIdOrIds}`
+  }`
+
+  const averageRatings: AverageRating[] = await prisma.$queryRaw`
     SELECT 
       "movieId",
       AVG("rating") AS "averageRating"
     FROM "Review"
-    WHERE "movieId" = ANY (ARRAY[${movieIds}]::integer[])
+    WHERE ${condition}
     GROUP BY "movieId";
   `
 
-  const reviews: Review[] | null = await prisma.review.findMany({
+  return averageRatings
+}
+
+async function getMovieReviews(movieIds: number[]): Promise<Review[]> {
+  const reviews: Review[] = await prisma.review.findMany({
     select: {
       id: true,
-      comment: true,
       movieId: true,
       userId: true,
     },
@@ -134,7 +142,16 @@ export async function getMovies(page: number): Promise<MovieDetail[] | null> {
     },
   })
 
-  const movieDetails = movieResults.map((movie) => {
+  return reviews
+}
+
+function buildMovieDetails(
+  movies: MovieResult[],
+  favorites: Favorite[],
+  reviews: Review[],
+  averageRatings: AverageRating[],
+): MovieDetail[] {
+  const movieDetails = movies.map((movie) => {
     const isFavorite = favorites.some((favorite) => favorite.movieId === movie.id)
     const movieReviews = reviews.filter((review) => review.movieId === movie.id)
     const averageRating = averageRatings.find((rating) => rating.movieId === movie.id)
@@ -153,83 +170,25 @@ export async function getMovies(page: number): Promise<MovieDetail[] | null> {
   return movieDetails
 }
 
-export async function getFavoriteMovies(): Promise<MovieDetail[] | null> {
-  const session = await verifySession()
-  if (!session.isAuth) return null
+async function getFavorites(
+  userId: number,
+  movieId?: number,
+  movieIds?: number[],
+): Promise<Favorite[]> {
+  const whereClause: Record<string, unknown> = { userId }
 
-  const favorites = await prisma.favorite.findMany({
-    select: {
-      id: true,
-      movieId: true,
-      userId: true,
-    },
-    where: {
-      userId: session.userId,
-    },
-  })
-
-  const favoriteMovieIds = favorites.map((favorite) => favorite.movieId)
-  let favoriteMovies: MovieResult[] = []
-  try {
-    const options = {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`,
-      },
-    }
-
-    const movieRequests = favoriteMovieIds.map((movieId) =>
-      fetch(`https://api.themoviedb.org/3/movie/${movieId}`, {
-        ...options,
-        cache: 'force-cache',
-      }).then((res) => res.json()),
-    )
-    const movies: MovieResult[] = await Promise.all(movieRequests)
-    favoriteMovies = movies
-  } catch (error) {
-    console.error('Error fetching movies:', error)
+  if (movieId) {
+    whereClause.movieId = movieId
+  } else if (movieIds) {
+    whereClause.movieId = { in: movieIds }
   }
-  if (favoriteMovies.length === 0) return null
 
-  const averageRatings: MovieReviewFromDB[] = await prisma.$queryRaw`
-    SELECT 
-      "movieId",
-      AVG("rating") AS "averageRating"
-    FROM "Review"
-    WHERE "movieId" = ANY (ARRAY[${favoriteMovieIds}]::integer[])
-    GROUP BY "movieId";
-  `
-
-  const reviews: Review[] | null = await prisma.review.findMany({
+  return prisma.favorite.findMany({
     select: {
       id: true,
-      comment: true,
       movieId: true,
       userId: true,
     },
-    where: {
-      movieId: {
-        in: favoriteMovieIds,
-      },
-    },
+    where: whereClause,
   })
-
-  const movieDetails = favoriteMovies.map((movie) => {
-    const isFavorite = favorites.some((favorite) => favorite.movieId === movie.id)
-    const movieReviews = reviews.filter((review) => review.movieId === movie.id)
-    const averageRating = averageRatings.find((rating) => rating.movieId === movie.id)
-    const averageRatingNumber = averageRating
-      ? Math.ceil(averageRating.averageRating.toNumber() * 10) / 10
-      : undefined
-
-    return {
-      ...movie,
-      isFavorite,
-      averageRating: averageRatingNumber,
-      reviews: movieReviews,
-    }
-  })
-
-  return movieDetails
 }
--- src/features/movie/schema.ts
+++ src/features/movie/schema.ts
@@ -3,3 +3,7 @@ import { z } from 'zod'
 export const searchParamsSchema = z.object({
   page: z.string().regex(/^\d+$/).transform(Number).optional(),
 })
+
+export const pathParamsSchema = z.object({
+  movieId: z.string().regex(/^\d+$/).transform(Number),
+})
